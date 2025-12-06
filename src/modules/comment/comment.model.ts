11296f5f90743d71391feaa9d3be844c95063eb6
rukonbd1 <rukonbd.dev@gmail.com>
2025-12-06 20:54:26 +0600

    comment model added

---
+++ src/modules/comment/comment.model.ts
@@ -0,0 +1,14 @@
+// modules/comment/comment.model.js
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    content: { type: String, required: true, trim: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, index: true },
+    replyCount: { type: Number, default: 0 },
+    likeCount: { type: Number, default: 0 },
+    dislikeCount: { type: Number, default: 0 },
+    createdAt: { type: Date, default: Date.now, index: true },
+    updatedAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("Comment", commentSchema);
