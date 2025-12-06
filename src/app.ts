31ddf0dafc382d140b1e7477949bd9e9457e54a2
rukonbd1 <rukonbd.dev@gmail.com>
2025-12-06 23:19:16 +0600

    main router added

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,15 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import env from "./config/env.config";
 import { errorHandler } from "./middlewares/error.middleware";
+import router from "./router";
 
 const app = express();
 
 app.use(cors({ origin: env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
+
+app.use('/api', router);
 
 app.get("/", (req, res) => {
     res.send("Backend is running!");
