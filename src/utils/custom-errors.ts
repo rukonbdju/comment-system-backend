ed2fdfd59f0ffdc4a49e0cd9fb7d6969d15409ba
rukonbd1 <rukonbd.dev@gmail.com>
2025-12-06 21:59:08 +0600

    custom error classes

---
+++ src/utils/custom-errors.ts
@@ -0,0 +1,24 @@
+export class BaseError extends Error {
+    // Default to 500 Internal Server Error
+    public readonly statusCode: number = 500;
+
+    constructor(message: string, statusCode: number) {
+        super(message);
+        this.statusCode = statusCode;
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+// when resource not found
+export class NotFoundError extends BaseError {
+    constructor(message: string = 'Resource not found') {
+        super(message, 404);
+    }
+}
+
+// when resource already exist
+export class ConflictError extends BaseError {
+    constructor(message: string = 'Conflict with existing resource') {
+        super(message, 409);
+    }
+}
